3ad0a5488e2d29049561ae3efb550dae2e7d55e0
Peter <pete225848@gmail.com>
2021-05-17 15:44:02 -0700

    added pokemon js file wiith 10 pokemon and pokemon array

---
+++ pokemon/pokemon.js
@@ -0,0 +1,95 @@
+const flaaffy = {
+    id: 0,
+    name: 'Flaaffy',
+    image: 'Flaaffy.png',
+    type: 'Electric',
+    captured: 0,
+    encountered: 0
+};
+const gengar = {
+    id: 1,
+    name: 'Gengar',
+    image: 'Gengar.webp',
+    type: ['Ghost', 'Poison'],
+    captured: 0,
+    encountered: 0
+};
+const piplup = {
+    id: 2,
+    name: 'Piplup',
+    image: 'Piplup.png',
+    type: 'Water',
+    captured: 0,
+    encountered: 0
+};
+const dragonite = {
+    id: 3,
+    name: 'Dragonite',
+    image: 'Dragonite.png',
+    type: ['Dragon', 'Flying'],
+    captured: 0,
+    encountered: 0
+};
+const zapdos = {
+    id: 4,
+    name: 'Zapdos',
+    image: 'Zapdos.png',
+    type: 'Electric',
+    captured: 0,
+    encountered: 0
+};
+const slugma = {
+    id: 5,
+    name: 'Slugma',
+    image: 'Slugma.png',
+    type: 'Fire',
+    captured: 0,
+    encountered: 0
+};
+const mrMime = {
+    id: 6,
+    name: 'Mr Mime',
+    image: 'Mr-Mime.png',
+    type: ['Psychic', 'Fairy'],
+    captured: 0,
+    encountered: 0
+};
+const roselia = {
+    id: 7,
+    name: 'Roselia',
+    image: 'Roselia.png',
+    type: ['Grass', 'Poison'],
+    captured: 0,
+    encountered: 0
+};
+const blazekin = {
+    id: 8,
+    name: 'Blazekin',
+    image: 'Blazekin.png',
+    type: ['Fire', 'Fighting'],
+    captured: 0,
+    encountered: 0
+};
+const arceus = {
+    id: 9,
+    name: 'Arceus',
+    image: 'Arceus.png',
+    type: 'Normal',
+    captured: 0,
+    encountered: 0
+};
+
+const pokemon = [
+    flaaffy,
+    gengar,
+    piplup,
+    dragonite,
+    zapdos,
+    slugma,
+    mrMime,
+    roselia,
+    blazekin,
+    arceus
+];
+
+export default pokemon;
